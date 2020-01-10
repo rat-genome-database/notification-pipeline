4c063ed209a436ba95c8ae155e17b75fb3ba7ec1
tutajm <mtutaj@mcw.edu>
2020-01-10 11:40:05 -0600

    tune up of master script

--- src/main/dist/notificationPipeline.sh
+++ src/main/dist/notificationPipeline.sh
@@ -8,11 +8,10 @@ SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 cd $APP_HOME
 
 if [ "$SERVER" == "REED" ]; then
-  ./_run.sh lomu
+  ./_run.sh lomu "$@"
 else
   # dev server: run on dev database and send all notifications to mtutaj@mcw.edu
-  ./_run.sh default_db  debug=mtutaj@mcw.edu
+  ./_run.sh default_db  debug=mtutaj@mcw.edu "$@"
 fi
-
 
 
