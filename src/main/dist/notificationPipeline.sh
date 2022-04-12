8dfb6fe62af8b9ee995abd29259830b51cdc54fe
tutajm <mtutaj@mcw.edu>
2022-04-12 10:26:07 -0500

    updated prod db name

--- src/main/dist/notificationPipeline.sh
+++ src/main/dist/notificationPipeline.sh
@@ -8,7 +8,7 @@ SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 cd $APP_HOME
 
 if [ "$SERVER" == "REED" ]; then
-  ./_run.sh lomu "$@"
+  ./_run.sh culber "$@"
 else
   # dev server: run on dev database and send all notifications to mtutaj@mcw.edu
   ./_run.sh default_db  debug=mtutaj@mcw.edu "$@"
